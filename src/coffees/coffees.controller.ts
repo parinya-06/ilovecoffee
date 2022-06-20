dac01da5d27111aacc4577d403b31dbf5856a3a8
parinya-06 <parinya.ru.61@ubu.ac.th>
2022-06-20 14:00:06 +0700

    update coffee.service

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,9 +1,11 @@
 import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, Patch, Post, Query, Res } from '@nestjs/common';
 import { response } from 'express';
 import { send } from 'process';
+import { CoffeesService } from './coffees.service';
 
 @Controller('coffees')
 export class CoffeesController {
+    constructor(private readonly coffeesService: CoffeesService) { }
     // @Get()
     // findAll(@Res() response) {
     //     // return 'This action returns all coffees';
@@ -13,32 +15,34 @@ export class CoffeesController {
     //ค้นหา
     @Get()
     findAll(@Query() paginationQuery) {
-        const { name, value } = paginationQuery
-        console.log(name);
-        console.log(value);
-        
-        return `return 'This action returns all coffees, name:${name} value:${value}`
+        // const { name, value } = paginationQuery
+        // return `return 'This action returns all coffees, name:${name} value:${value}`
+        return this.coffeesService.findAll();
 
     }
 
     @Get(':id')
     findOne(@Param('id') id: string) {
-        return `This action returns #${id} coffees`;
+        // return `This action returns #${id} coffees`;
+        return this.coffeesService.fineOne(id);
     }
 
     @Post()
     // @HttpCode(HttpStatus.GONE)
     create(@Body() body) {
-        return body;
+        // return body;
+        return this.coffeesService.create(body);
     }
 
     @Patch(':id')
     update(@Param('id') id: string, @Body() body) {
-        return `This action update #${id} coffees`;
+        // return `This action update #${id} coffees`;
+        return this.coffeesService.update(id, body)
     }
 
     @Delete(':id')
     remove(@Param('id') id: string) {
-        return `This action remove #${id} coffees`;
+        // return `This action remove #${id} coffees`;
+        return this.coffeesService.remove(id)
     }
 }
