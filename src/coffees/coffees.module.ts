09cf394312dfe2a9e72e63839c88db057e73b970
parinya-06 <parinya.ru.61@ubu.ac.th>
2022-06-22 15:07:50 +0700

    class Providers

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -8,6 +8,9 @@ import { Event } from 'src/events/entities/event.entity';
 import { COFFEE_BRANDS } from './coffees.constants';
 
 class MockCoffeesService { }
+class ConfigService { }
+class DevelopmentConfigService { }
+class ProductionConfigService { }
 
 @Module({
     imports: [TypeOrmModule.forFeature([Coffee, Flavor, Event])],
@@ -16,6 +19,13 @@ class MockCoffeesService { }
     // providers: [{ provide: CoffeesService, useValue: new MockCoffeesService() }],
     providers: [
         CoffeesService,
+        {
+            provide: ConfigService,
+            useClass:
+                process.env.NODE_ENV === 'development'
+                    ? DevelopmentConfigService
+                    : ProductionConfigService
+        },
         {
             provide: COFFEE_BRANDS, useValue: ['buddy brew', 'nescafe'],
         }
