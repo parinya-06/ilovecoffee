fd5db3204975680fde171a488d19c2bec47d81ec
parinya-06 <parinya.ru.61@ubu.ac.th>
2022-06-22 10:13:19 +0700

    add entity

--- src/coffees/entities/coffee.entity.ts
+++ src/coffees/entities/coffee.entity.ts
@@ -1,6 +1,25 @@
+import { Column, Entity, JoinTable, ManyToMany, PrimaryGeneratedColumn } from "typeorm";
+import { Flavor } from "./flavor.entity";
+
+@Entity()//sql table === 'coffee'
 export class Coffee {
+
+    @PrimaryGeneratedColumn()
     id: number;
+
+    @Column()
     name: string;
+
+    @Column()
     brand: string;
+
+    @JoinTable()
+    @ManyToMany(
+        type => Flavor,
+        flavor => flavor.coffees,
+        {
+            cascade: true,//['insert']
+        },
+    )
     flavors: string[];
 }
