999c3d673dc2aeacd4b496361140f004cb61cd95
parinya-06 <parinya.ru.61@ubu.ac.th>
2022-06-22 15:27:23 +0700

    Leverage Async Providers

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -6,6 +6,7 @@ import { Coffee } from './entities/coffee.entity'
 import { Flavor } from './entities/flavor.entity'
 import { Event } from 'src/events/entities/event.entity';
 import { COFFEE_BRANDS } from './coffees.constants';
+import { Connection } from 'typeorm';
 
 class MockCoffeesService { }
 class ConfigService { }
@@ -37,13 +38,23 @@ export class CoffeeBrandsFactory {
         // {
         //     provide: COFFEE_BRANDS, useValue: ['buddy brew', 'nescafe'],
         // }
-        CoffeeBrandsFactory,
+        // CoffeeBrandsFactory,
+        // {
+        //     provide: COFFEE_BRANDS,
+        //     useFactory: (brandsFactory: CoffeeBrandsFactory) =>
+        //         brandsFactory.create(),
+        //     inject: [CoffeeBrandsFactory]
+        // }
         {
             provide: COFFEE_BRANDS,
-            useFactory: (brandsFactory: CoffeeBrandsFactory) =>
-                brandsFactory.create(),
-            inject: [CoffeeBrandsFactory]
-        }
+            useFactory: async (connection: Connection): Promise<string[]> => {
+                // const coffeeBrands = await connection.query('SELECT * ...');
+                const coffeeBrands = await Promise.resolve(['buddy brew', 'nescafe']);
+                console.log('[!] Async factory');
+                return coffeeBrands;
+            },
+            inject: [Connection],
+        },
     ],
     exports: [CoffeesService],
 })
