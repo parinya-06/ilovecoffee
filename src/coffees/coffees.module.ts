eb45ecb330055826cbacfe78c7371f0db196e582
parinya-06 <parinya.ru.61@ubu.ac.th>
2022-06-22 15:01:31 +0700

    Non-class-based Provider Tokens

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -5,6 +5,7 @@ import { TypeOrmModule } from '@nestjs/typeorm'
 import { Coffee } from './entities/coffee.entity'
 import { Flavor } from './entities/flavor.entity'
 import { Event } from 'src/events/entities/event.entity';
+import { COFFEE_BRANDS } from './coffees.constants';
 
 class MockCoffeesService { }
 
@@ -12,7 +13,13 @@ class MockCoffeesService { }
     imports: [TypeOrmModule.forFeature([Coffee, Flavor, Event])],
     controllers: [CoffeesController],
     // providers: [CoffeesService],
-    providers: [{ provide: CoffeesService, useValue: new MockCoffeesService() }],
+    // providers: [{ provide: CoffeesService, useValue: new MockCoffeesService() }],
+    providers: [
+        CoffeesService,
+        {
+            provide: COFFEE_BRANDS, useValue: ['buddy brew', 'nescafe'],
+        }
+    ],
     exports: [CoffeesService],
 })
 export class CoffeesModule { }
--- src/coffees/coffees.service.ts
+++ src/coffees/coffees.service.ts
@@ -1,6 +1,7 @@
 import {
     HttpException,
     HttpStatus,
+    Inject,
     Injectable,
     NotFoundException,
 } from '@nestjs/common'
@@ -8,6 +9,7 @@ import { InjectRepository } from '@nestjs/typeorm'
 import { PaginationQueryDto } from 'src/common/dto/pagination.dto'
 import { Event } from 'src/events/entities/event.entity'
 import { Repository, Connection } from 'typeorm'
+import { COFFEE_BRANDS } from './coffees.constants'
 import { CreateCoffeeDto } from './dto/create-coffee.dto'
 import { UpdateCoffeeDto } from './dto/update-coffee.dto'
 import { Coffee } from './entities/coffee.entity'
@@ -21,7 +23,11 @@ export class CoffeesService {
         @InjectRepository(Flavor)
         private readonly flavorRepository: Repository<Flavor>,
         private readonly connection: Connection,
-    ) { }
+        @Inject(COFFEE_BRANDS) coffeeBrands: string[],
+    ) {
+        console.log('coffeeBrands', '=', coffeeBrands);
+
+    }
 
     findAll(paginationQuery: PaginationQueryDto) {
         const { limit, offset } = paginationQuery;
