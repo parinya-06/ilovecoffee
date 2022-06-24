4968fbd378e8c1c1190a4e544b389335f6a25abb
parinya-06 <parinya.ru.61@ubu.ac.th>
2022-06-24 12:08:57 +0700

    add pagination

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,4 +1,5 @@
 import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, Patch, Post, Query, Res } from '@nestjs/common';
+import { PaginationQueryDto } from 'src/common/dto/pagination-query.dto';
 import { CoffeesService } from './coffees.service';
 import { CreateCoffeeDto } from './dto/create-coffee.dto';
 import { UpdateCoffeeDto } from './dto/update-coffee.dto';
@@ -9,10 +10,10 @@ export class CoffeesController {
 
     //ค้นหา
     @Get()
-    findAll(@Query() paginationQuery) {
+    findAll(@Query() paginationQuery: PaginationQueryDto) {
         // const { limit, offset } = paginationQuery
         // return `return 'This action returns all coffees, name:${name} value:${value}`
-        return this.coffeesService.findAll();
+        return this.coffeesService.findAll(paginationQuery);
     }
 
     @Get(':id')
