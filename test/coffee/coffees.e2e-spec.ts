ff48af1c62712078b14d799b04b38035c40c1a04
parinya-06 <parinya.ru.61@ubu.ac.th>
2022-06-24 10:47:00 +0700

    update testing

---
+++ test/coffee/coffees.e2e-spec.ts
@@ -0,0 +1,70 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { HttpStatus, INestApplication, ValidationPipe } from '@nestjs/common';
+import { CoffeesModule } from '../../src/coffees/coffees.module'
+import { TypeOrmModule } from '@nestjs/typeorm';
+import * as request from 'supertest';
+import { CreateCoffeeDto } from 'src/coffees/dto/create-coffee.dto';
+
+describe('[Feature] Coffees - /coffees', () => {
+    const coffee = {
+        name: "coffee #1",
+        brand: "Buddy Brew123",
+        flavors: ["chocolate", "valila"]
+    };
+    let app: INestApplication;
+
+    beforeAll(async () => {
+        const moduleFixture: TestingModule = await Test.createTestingModule({
+            imports: [CoffeesModule,
+                TypeOrmModule.forRoot({
+                    type: 'postgres',
+                    host: 'localhost',
+                    port: 5433,
+                    username: 'postgres',
+                    password: 'pass123',
+                    database: 'postgres',
+                    autoLoadEntities: true,
+                    synchronize: true,
+                })
+            ],
+        }).compile();
+
+        app = moduleFixture.createNestApplication();
+        app.useGlobalPipes(
+            new ValidationPipe({
+                whitelist: true,
+                transform: true,
+                forbidNonWhitelisted: true,
+                transformOptions: {
+                    enableImplicitConversion: true,
+                },
+            }),
+        );
+
+        await app.init();
+    });
+
+    it('Create [POST /]', () => {
+        return request(app.getHttpServer())
+            .post('/coffees ')
+            .send(coffee as CreateCoffeeDto)
+            .expect(HttpStatus.CREATED)
+            .then(({ body }) => {
+                const expectedcoffee = jasmine.objectContaining({
+                    ...coffee,
+                    flavors: jasmine.arrayContaining(
+                        coffee.flavors.map(name => jasmine.objectContaining({ name })),
+                    )
+                });
+                expect(body).toEqual(expectedcoffee);
+            })
+    });
+    it.todo('Get all [GET /]');
+    it.todo('Get one [GET /:id] ');
+    it.todo('Update one [PATCH /:id]');
+    it.todo('Delete one [DELETE /:id]');
+
+    afterAll(async () => {
+        await app.close();
+    })
+});
