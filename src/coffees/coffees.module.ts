9db49a24a7fda876fbb28a08c8f7a3ea3718ec65
parinya-06 <parinya.ru.61@ubu.ac.th>
2022-06-24 11:37:41 +0700

    add mongoosedb

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -1,8 +1,16 @@
 import { Module } from '@nestjs/common';
 import { CoffeesController } from './coffees.controller';
 import { CoffeesService } from './coffees.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Coffee, CoffeeSchema } from './entities/coffee.entity';
 
 @Module({
+    imports: [MongooseModule.forFeature([
+        {
+            name: Coffee.name,
+            schema: CoffeeSchema
+        }
+    ])],
     controllers: [CoffeesController],
     providers: [CoffeesService]
 })
